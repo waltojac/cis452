d895dc17a83929f8290095a29c23c13c417cc4ec
Jacob Walton <waltojac@mail.gvsu.edu>
2018-01-22 11:42:43 -0500

    Inserted comments for array creation.

--- Labs/Lab2/program.c
+++ Labs/Lab2/program.c
@@ -9,11 +9,13 @@
 
 int main ()
 {
+  //variables
   char input[64];
   char* token;
   int count = 1;
   int end = 1;
   char* args[20];
+
   //User Prompt
   printf("Enter command: ");
 
@@ -22,17 +24,19 @@ int main ()
 
   //========Testing==========
 
-  //Parse input
+  //Parse first input
   args[0] = strtok(input, " ");
-  printf("First token: %s\n", token);
+
   //Calls to strtok after the first need to have
   //NULL as the string. Token will return null if
-  //there are no more.
-  //puts arguments into an array
+  //there are no more.This loop puts rest of arguments
+  //into an array.
   while (end)
   {
     args[count] = strtok (NULL, " ");
     count++;
+    //Check if all arguments have been tokenized.
+    //strtok() will return 0 if there is no string left.
     if (args[count - 1] == 0)
     {
       count -= 2;
@@ -40,6 +44,8 @@ int main ()
     }
   }
 
+  //print for testing of array
+  printf("\nFirst: %s ", args[0]);
   printf("\nSec: %s ", args[1]);
   printf("\nThird: %s ", args[2]);
 
