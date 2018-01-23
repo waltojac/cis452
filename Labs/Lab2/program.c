d90d9ee0aab2823fc3710c0afd3d1dc54f9713f3
lafeldtb <ben@lafeldt.com>
2018-01-23 15:00:34 -0500

    Almost works, errors on even command arguments, something in the push

--- Labs/Lab2/program.c
+++ Labs/Lab2/program.c
@@ -6,12 +6,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <mcheck.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <errno.h>
 
 int array_push(char*** arr, int size, char* c);
+int free_arr(char*** arr, int size);
 
 int main (){
+	mtrace();
+
 	char input[64];
 	char** cmd_args = NULL;
+	pid_t pid;
+	int status;
 
 	//User Prompt
 	printf("Enter command: ");
@@ -24,7 +34,7 @@ int main (){
 	int count = 1;
 
 	//empty array of cmd arguments
-	cmd_args = (char**)malloc(sizeof(char*)*0);
+	cmd_args = (char**)malloc(sizeof(char*)*1);
 
 	//If the current token exits, push onto array and get next one
 	while(token != NULL) {
@@ -33,7 +43,26 @@ int main (){
 		token = strtok(NULL, " \n"); 
 	}
 
-	free(cmd_args);
+	//Spawn child process
+	pid = fork();
+	if(pid < 0)
+	{
+		fprintf(stderr, "An error occurred while forking");
+		return -1;
+	} else if (pid) {
+		waitpid(pid, &status, 0);
+	} else {
+		printf("cmd_args[2]: %s\n", cmd_args[2]);
+		execvp(cmd_args[0], &cmd_args[0]);
+		/*
+		printf("Exec fail\n");
+		*/
+		printf("Error: %s\n", strerror(errno));
+		exit(errno);
+	}
+
+
+	free_arr(&cmd_args, count);
 	return 0;
 }
 
@@ -43,7 +72,7 @@ int array_push(char*** arr, int size, char* c) {
 	*arr = malloc(sizeof(char*)*size);
 	
 	int i;
-	for(i = 0; i < size-1; i++)
+	for(i = 0; i < (size-1); i++)
 	{
 		(*arr)[i] = (char*)malloc(sizeof(char*));
 		strcpy((*arr)[i], tmp[i]);
@@ -51,5 +80,16 @@ int array_push(char*** arr, int size, char* c) {
 	(*arr)[size-1] = (char*)malloc(sizeof(char*));
 	strcpy((*arr)[size-1], c);
 	free(tmp);
+	return 1;
+}
+
+int free_arr(char*** arr, int size) {
+	int i;
+	for(i = 0; i < size-1; i++)
+	{
+		char* currentPtr = (*arr)[i];
+		free(currentPtr);
+	}
+	//free(arr);
 	return 1;
 }
