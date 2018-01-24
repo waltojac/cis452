b543b4d5352cd6f8b41527d74a46ff3a79acae74
Jacob Walton <waltojac@eos11.cis.gvsu.edu>
2018-01-24 16:08:26 -0500

    Lab 2 Program complete

--- Labs/Lab2/program.c
+++ Labs/Lab2/program.c
@@ -11,6 +11,10 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 #include <errno.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+
+
 
 #define ARRSIZE 64
 
@@ -23,8 +27,12 @@ int main (){
 	char* cmd_args[ARRSIZE];
 	pid_t pid;
 	int status;
+	long lastusec = 0;
+	long lastsec = 0;
+	long lastnivcsw = 0;
 
 	while(1) {
+		struct rusage *resourceUsage = (struct rusage*)malloc(sizeof(struct rusage));
 
 		//User Prompt
 		printf("Enter command: ");
@@ -65,7 +73,19 @@ int main (){
 			fprintf(stderr, "An error occurred while forking");
 			return -1;
 		} else if (pid) {
+			//getrusage(pid, resourceUsage);
+			//long time = 0;
+			//getrusage(pid, resourceUsage);
+			//time = resourceUsage->ru_utime.tv_usec;
 			waitpid(pid, &status, 0);
+			getrusage(RUSAGE_CHILDREN, resourceUsage);
+			//time = resourceUsage->ru_utime.tv_usec - time;
+			//printf("%ul\n", resourceUsage->ru_utime.tv_usec);
+			printf("time: %lu Seconds, %lu Microseconds\n", resourceUsage->ru_utime.tv_sec - lastsec, resourceUsage->ru_utime.tv_usec - lastusec);
+			printf("Involuntary Context Switches: %lu\n", resourceUsage->ru_nivcsw - lastnivcsw);
+			lastusec = resourceUsage->ru_utime.tv_usec;
+			lastsec = resourceUsage->ru_utime.tv_sec;
+			lastnivcsw = resourceUsage->ru_nivcsw;
 		} else {
 			if (execvp(cmd_args[0], &(cmd_args[0])) < 0) {
 				exit(errno);
