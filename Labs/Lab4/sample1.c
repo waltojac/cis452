99fb0c2bdc8d17c298262df1cda9316b35d50dcf
lafeldtb <ben@lafeldt.com>
2018-02-01 14:34:13 -0500

    After lab 4 time

--- Labs/Lab4/sample1.c
+++ Labs/Lab4/sample1.c
@@ -8,20 +8,21 @@
 void* do_greeting (void* arg); 
 int main() 
 { 
- pthread_t thread1;  // thread ID holder 
- int status;         // captures any error code
+	pthread_t thread1;  // thread ID holder 
+	int status;         // captures any error code
 
-// create and start a thread executing the "do_greeting()" function 
-     if ((status = pthread_create (&thread1, NULL,  do_greeting, NULL)) != 0) { 
-             fprintf (stderr, "thread create error %d: %s\n", status, strerror(status)); 
-                     exit (1); 
-                         }
+	// create and start a thread executing the "do_greeting()" function 
+	if ((status = pthread_create (&thread1, NULL,  do_greeting, NULL)) != 0) { 
+		fprintf (stderr, "thread create error %d: %s\n", status, strerror(status)); 
+		exit (1); 
+	}
+	sleep(2);
 
-                             return 0; 
-                             }
-                             void* do_greeting (void* arg) 
-                             { 
-                                 sleep(1); 
-                                     printf ("Thread version of Hello, world.\n"); 
-                                         return arg; 
-                                         }
+	return 0; 
+}
+void* do_greeting (void* arg) 
+{ 
+	sleep(1); 
+	printf ("Thread version of Hello, world.\n"); 
+	return arg; 
+}
--- Labs/Lab4/sample2.c
+++ Labs/Lab4/sample2.c
@@ -1,4 +1,4 @@
-nclude <pthread.h> 
+#include <pthread.h> 
 #include <iostream> 
 #include <errno.h> 
 #include <unistd.h> 
@@ -12,53 +12,54 @@ void* do_greeting2 (void* arg);
 // // returns :       a pointer to whatever was passed in to arg 
 // // side effects:  prints a greeting
 //
- int main() 
- { 
-     pthread_t thread1, thread2;  // thread ID's 
-         void *result1, *result2;     // return values 
-             int status;
+int main() 
+{ 
+	pthread_t thread1, thread2;  // thread ID's 
+	void *result1, *result2;     // return values 
+	int status;
 
- // create and start two threads; both executing the "do_greeting2" function 
-//             // pass the threads a pointer to NULL as their argument 
-                 if ((status = pthread_create (&thread1, NULL,  do_greeting2, NULL)) != 0) { 
-                         cerr << "thread create error: " << strerror(status) << endl; 
-                                 exit (1); 
-                                     } 
-                                         if ((status = pthread_create (&thread2, NULL,  do_greeting2, NULL)) != 0) { 
-                                                 cerr << "thread create error: " << strerror(status) << endl; 
-                                                         exit (1); 
-                                                             }
+	// create and start two threads; both executing the "do_greeting2" function 
+	//             // pass the threads a pointer to NULL as their argument 
+	if ((status = pthread_create (&thread1, NULL,  do_greeting2, NULL)) != 0) { 
+		cerr << "thread create error: " << strerror(status) << endl; 
+		exit (1); 
+	} 
+	if ((status = pthread_create (&thread2, NULL,  do_greeting2, NULL)) != 0) { 
+		cerr << "thread create error: " << strerror(status) << endl; 
+		exit (1); 
+	}
 
-                                                             // join with the threads (wait for them to terminate);  get their return vals 
-                                                                 if ((status = pthread_join (thread1, &result1)) != 0) { 
-                                                                         cerr << "join error: " << strerror(status) << endl; 
-                                                                                 exit (1); 
-                                                                                     } 
-                                                                                         if ((status = pthread_join (thread2, &result2)) != 0) { 
-                                                                                                 cerr << "join error: " << strerror(status) << endl; 
-                                                                                                         exit (1); 
-                                                                                                             }
+	// join with the threads (wait for them to terminate);  get their return vals 
+	if ((status = pthread_join (thread1, &result1)) != 0) { 
+		cerr << "join error: " << strerror(status) << endl; 
+		exit (1); 
+	} 
+	if ((status = pthread_join (thread2, &result2)) != 0) { 
+		cerr << "join error: " << strerror(status) << endl; 
+		exit (1); 
+	}
 
-                                                                                                             // threads return what they were passed (i.e. NULL) 
-                                                                                                                 if (result1 != NULL || result2 != NULL) { 
-                                                                                                                         cerr << "bad result"  << endl; 
-                                                                                                                                 exit (1); 
-                                                                                                                                     } 
-                                                                                                                                         return 0; 
-                                                                                                                                         }
+	// threads return what they were passed (i.e. NULL) 
+	if (result1 != NULL || result2 != NULL) { 
+		cerr << "bad result"  << endl; 
+		exit (1); 
+	} 
+	return 0; 
+}
 
-                                                                                                                                         void* do_greeting2 (void* arg) 
-                                                                                                                                         { 
-                                                                                                                                             int val = rand() % 2;
+void* do_greeting2 (void* arg) 
+{ 
+	int val = rand() % 2;
 
-                                                                                                                                             // print out message based on val 
-                                                                                                                                                 for (int loop = 0;  loop < 10;  loop++) { 
-                                                                                                                                                         if (!val) 
-                                                                                                                                                                     cout << "Hello "; 
-                                                                                                                                                                             else 
-                                                                                                                                                                                         cout << "World\n "; 
-                                                                                                                                                                                             } 
-                                                                                                                                                                                                 return arg; 
-                                                                                                                                                                                                 }
+	// print out message based on val 
+	for (int loop = 0;  loop < 10;  loop++) { 
+		sleep(1);
+		if (!val) 
+			cout << "Hello "; 
+		else 
+			cout << "World\n "; 
+	} 
+	return arg; 
+}
 
 
