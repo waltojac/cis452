76f8ea07c40d02c9fba095fc7eba74044ce980a4
Jacob Walton <waltojac@eos11.cis.gvsu.edu>
2018-02-05 15:29:45 -0500

    Extra credit finished.

--- Labs/Lab4/program.c
+++ Labs/Lab4/program.c
@@ -18,12 +18,22 @@
 void* worker(void* args);
 void sigHandler(int sigNum);
 
+//usage statistics
 int requests;
+float totalAccessTime;
+
 char* input;
 
-int main(){
+//thread mutex variable
+pthread_mutex_t lock;
 
+
+int main(){
+	
+	//usage statistics initialization
 	requests = 0;
+	totalAccessTime = 0;
+	
 
 	//Install signal   handlers
 	signal(SIGINT, sigHandler);
@@ -31,7 +41,12 @@ int main(){
 	//variables
 	pthread_t thread1;  // thread ID holder 
 	int status;         // captures any error code
-	
+
+	if (pthread_mutex_init(&lock, NULL) != 0){
+		printf("\n mutex init failed.\n");
+		return 1;
+	}
+
 	while(1){
 		//user input
 		input = (char*)malloc(sizeof(char)*64);
@@ -60,18 +75,28 @@ int main(){
 }
 
 void* worker(void* args){
+	pthread_mutex_lock(&lock);
+
 	char * data = (char*)args;
 	//Desired file found
 	int prob = rand() % 10;
+	
+	//for 20% probability
 	if(prob < 2) {
 		printf("File \"%s\" not in cache, searching Hard Drive...\n", data);
 		int sleepnum = rand() % 4 + 7;
 		sleep(sleepnum);
+		totalAccessTime += (float)sleepnum;
 		printf("File \"%s\" found in Hard Drive\n", data);
-	} else {
+	} 
+	//for 80% probability
+	else {
 		sleep(1);
+		totalAccessTime += 1.0;
 		printf("File \"%s\" found in cache\n", data);
 	}
+	pthread_mutex_unlock(&lock);
+
 	return args;
 }
 
@@ -80,6 +105,7 @@ void sigHandler(int sigNum){
 		//graceful shutdown
 		free(input);
 		printf("\nTotal number of file requests received: %d\n", requests);
+		printf("Average file access time: %f seconds.\n", totalAccessTime/requests);
 		printf("\nShutting down... \n");
 		exit(0);
 	}
