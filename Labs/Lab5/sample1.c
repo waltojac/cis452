dc59bca3bb4c34f580fd92f9a9eb549155c512f6
lafeldtb <ben@lafeldt.com>
2018-02-08 14:26:43 -0500

    lab 5 questions 1-4 complete

--- Labs/Lab5/sample1.c
+++ Labs/Lab5/sample1.c
@@ -9,26 +9,40 @@
 
 int main () 
 { 
-   int shmId; 
-   char *shmPtr;
+	int shmId; 
+	char *shmPtr;
 
-   if ((shmId = shmget (IPC_PRIVATE, FOO, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0) { 
-      perror ("i can't get no..\n"); 
-      exit (1); 
-   } 
-   if ((shmPtr = shmat (shmId, 0, 0)) == (void*) -1) { 
-      perror ("can't attach\n"); 
-      exit (1); 
-   }
-   printf ("value a: %lu\t value b: %lu\n", (unsigned long) shmPtr, (unsigned long) shmPtr + FOO);
-   if (shmdt (shmPtr) < 0) { 
-      perror ("just can't let go\n"); 
-      exit (1); 
-   } 
-   if (shmctl (shmId, IPC_RMID, 0) < 0) { 
-      perror ("can't deallocate\n"); 
-      exit(1); 
-   }
+	if ((shmId = shmget (IPC_PRIVATE, FOO, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0) { 
+		perror ("i can't get no..\n"); 
+		exit (1); 
+	} 
+	if ((shmPtr = shmat (shmId, 0, 0)) == (void*) -1) { 
+		perror ("can't attach\n"); 
+		exit (1); 
+	}
+	printf ("value a: %lu\t value b: %lu\n", (unsigned long) shmPtr, (unsigned long) shmPtr + FOO);
 
-   return 0; 
+	//Edited for Question 4 ---------------
+	struct shmid_ds* buf;
+	buf = (struct shmid_ds*)malloc(sizeof(struct shmid_ds));
+	if(shmctl( shmId, IPC_STAT, buf) < 0)
+	{
+		perror("Error getting statistics\n");
+		exit(1);
+	}
+	printf("Shared memory segment size: %ld\n", buf->shm_segsz);
+	free(buf);
+
+	//-------------------------------------
+
+	if (shmdt (shmPtr) < 0) { 
+		perror ("just can't let go\n"); 
+		exit (1); 
+	} 
+	if (shmctl (shmId, IPC_RMID, 0) < 0) { 
+		perror ("can't deallocate\n"); 
+		exit(1); 
+	}
+
+	return 0; 
 }
