85c3b862caadd595120ba36b88c1180dc329955d
Jacob Walton <waltojac@eos11.cis.gvsu.edu>
2018-02-14 15:21:57 -0500

    Successful comm, need sigHandler still.

--- Labs/Lab5/reader.c
+++ Labs/Lab5/reader.c
@@ -1,23 +1,54 @@
 //Reader file
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 
 int main(){
 	int shmId;
 	char str[64];
 	char *shmPtr;
+	key_t set_key;
 
 	//get the shmId from the user
-	printf("Please enter the shared memory ID: \n");
+	/*
+	printf("Please enter the shared memory ID: ");
+	fflush(stdout);
 	fgets(str, 64, stdin);
 	shmId = atoi(str);
+	*/
+
+	//create key
+	set_key = ftok("/home/waltojac/Documents/cis452/Labs/Lab5", 'a');
+
+	if((shmId = shmget(set_key, 4096, S_IRUSR|S_IWUSR)) < 0 ) {
+		perror("Error creating shared memory\n");
+		exit(1);
+	}
+	
 
 	if ((shmPtr = shmat(shmId, 0, 0)) == (void*) -1){
 		perror("Can't attach\n");
 		exit(1);
 	}
 	
+	int offset = 8;
+	char * msg = shmPtr + offset;
+	int turn;
+
+	printf("Reader setup complete.\n");
+	while(1){
+		while(*shmPtr == 0);
+		//CS
+		//read from shmPtr
+		printf("%s", msg);
+		*shmPtr = 0;
+	}
+
 	printf("%s", shmPtr);
 
 	return 0;
--- Labs/Lab5/writer.c
+++ Labs/Lab5/writer.c
@@ -2,24 +2,53 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <unistd.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 
 int main (){
 
 	int shmId;
 	char *shmPtr;
+	key_t set_key;
 
-	if((shmId = shmget(IPC_PRIVATE, 4096, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0 ) {
+	//create key
+	set_key = ftok("/home/waltojac/Documents/cis452/Labs/Lab5", 'a');
+
+	if((shmId = shmget(set_key, 4096, IPC_CREAT|S_IRUSR|S_IWUSR)) < 0 ) {
 		perror("Error creating shared memory\n");
 		exit(1);
 	}
-	printf("Shared memory ID: %d\n", shmId);
 
 	if((shmPtr = shmat(shmId, 0, 0)) == (void*) -1) {
 		perror("Can't attach\n");
 		exit(1);
 	}
+
+	int offset = 8;
+	*(shmPtr + offset - 1) = '\0';
+	char * msg = shmPtr + offset;
+	*shmPtr = 0;
+	int turn;
+
+	while(1){
+		printf("\nTurn: %d", turn);
+		while (*shmPtr == 1);
+		printf("\nWriter in critical section...");
+		printf("\nEnter message: ");
+		fflush(stdout);
+		fgets(msg, 64, stdin);
+		//CS
+		//write to shmPtr
+
+		*shmPtr = 1;
+	}
+
+	
+
 
 	return 0;
 }
