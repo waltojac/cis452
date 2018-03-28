a084dc63918f6020578da2a7c9ae5c43aea4f467
Jacob Walton <waltojac@eos17.cis.gvsu.edu>
2018-03-28 15:23:50 -0400

    Lab 10 done.

--- Labs/Lab10/sample.c
+++ Labs/Lab10/sample.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 #define KB 1024
-#define LOOP 200
+#define LOOP 1
 
 int main()
 {
    int count, *intPtr;
 
-   long int i, j, dim = 2 * KB;
+   long int i, j, dim = 65 * KB;
 
    if ((intPtr = malloc (dim * dim * sizeof(int))) == 0) {
       perror ("totally out of space");
@@ -17,7 +17,7 @@ int main()
    for (count=1; count<=LOOP; count++)
       for (i=0; i<dim; i++)
          for (j=0; j<dim; j++)
-			intPtr[j * dim + i] = (i + j) % count;
+			intPtr[i * dim + j] = (i + j) % count;
    free (intPtr);
    return 0;
 }
