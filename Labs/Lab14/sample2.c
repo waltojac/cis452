60b789e669d893a5f3df41f1de4df59dfe7526e7
lafeldtb <ben@lafeldt.com>
2018-04-19 13:17:55 -0400

    Added lab14 stuff

---
+++ Labs/Lab14/sample2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char command[80] = "date +%A";
+    char name[32];
+
+    printf("Enter your nickname: ");
+    gets(name);
+    printf("Hi, %s.  Today is: ", name);
+    fflush(stdout);
+    system(command);
+    return 0;
+} 
